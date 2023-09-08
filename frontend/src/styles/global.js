cc29889c03762ab965e046e5f89c8b975df51778
SabinoDS <sabinoprog03@icloud.com>
2023-09-07 21:37:52 -0300

    first commit

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins', sans-serif;
+  }
+  
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-image: url(https://images.alphacoders.com/846/84631.jpg);
+    background-size: cover;
+  }
+`;
+
+export default Global;
